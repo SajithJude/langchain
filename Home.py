2bb769cd47ff21af3e466f6255fc5ca7b23b2671
Jude Sajith <judesajith.aj@gmail.com>
2023-03-23 04:43:20 -0400

    ssss

--- Home.py
+++ Home.py
@@ -3,21 +3,18 @@ from langchain.document_loaders import PyPDFLoader
 from langchain.indexes import VectorstoreIndexCreator
 import os
 import openai
-from langchain.embeddings import OpenAIEmbeddings
-embeddings = OpenAIEmbeddings()
-
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 loader = PyPDFLoader("content/Treasury Management Book .pdf")
-# pages = loader.load_and_split()
-index = VectorstoreIndexCreator(vectorstore_cls=Chroma, 
-    embedding=OpenAIEmbeddings()).from_loaders([loader])
+pages = loader.load_and_split()
+index = VectorstoreIndexCreator().from_loaders([loader])
 
 
 
+# query = st.text_input("Type your message here")
 qu = "What is treasury management"
 
-response = index.query(str(qu))
-st.write(qu)
+# response = 
+st.write(index.query(str(qu)))
