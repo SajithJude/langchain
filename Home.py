ec5b684b3872e7a15a9bc706b428fb6ee843916f
Jude Sajith <judesajith.aj@gmail.com>
2023-03-23 09:10:03 -0400

    sjsjsj

--- Home.py
+++ Home.py
@@ -1,30 +1,96 @@
-import streamlit as st
+# import streamlit as st
 
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.text_splitter import CharacterTextSplitter
-from langchain import OpenAI, VectorDBQA
+# from langchain.embeddings.openai import OpenAIEmbeddings
+# from langchain.vectorstores import Chroma
+# from langchain.text_splitter import CharacterTextSplitter
+# from langchain import OpenAI, VectorDBQA
 
-import os
-import openai
+# import os
+# import openai
+
+
+
+# from langchain.document_loaders import TextLoader
+# loader = TextLoader('content/inputenglish.txt')
+# documents = loader.load()
+# text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+# texts = text_splitter.split_documents(documents)
+
+# embeddings = OpenAIEmbeddings()
+# docsearch = Chroma.from_documents(texts, embeddings)
+# qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=docsearch)
+
+# query = st.text_input("Enter your question")
+# # qa.run(query)
+# # query = st.text_input("Type your message here")
+# # qu = 
+# if query is not None:
+# # response = 
+#     st.write(qa.run(query))
+
+
+import streamlit as st # import the Streamlit library
+from langchain.chains import LLMChain, SimpleSequentialChain # import LangChain libraries
+from langchain.llms import OpenAI # import OpenAI model
+from langchain.prompts import PromptTemplate # import PromptTemplate
+import os 
+
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# Set the title of the Streamlit app
+st.title("✅ Fact Checker  : Using LangChain `SimpleSequentialChain`")
 
-from langchain.document_loaders import TextLoader
-loader = TextLoader('content/inputenglish.txt')
-documents = loader.load()
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-texts = text_splitter.split_documents(documents)
+## If an API key has been provided, create an OpenAI language model instance
+if API:
+    llm = OpenAI(temperature=0.7, openai_api_key=API)
+else:
+    # If an API key hasn't been provided, display a warning message
+    st.warning("Enter your OPENAI API-KEY. Get your OpenAI API key from [here](https://platform.openai.com/account/api-keys).\n")
+# Add a text input box for the user's question
+user_question = st.text_input(
+    "Enter Your Question : ",
+    placeholder = "Cyanobacteria can perform photosynthetsis , are they considered as plants?",
+)
 
-embeddings = OpenAIEmbeddings()
-docsearch = Chroma.from_documents(texts, embeddings)
-qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=docsearch)
+# Generating the final answer to the user's question using all the chains
+if st.button("Tell me about it", type="primary"):
+    # Chain 1: Generating a rephrased version of the user's question
+    template = """{question}\n\n"""
+    prompt_template = PromptTemplate(input_variables=["question"], template=template)
+    question_chain = LLMChain(llm=llm, prompt=prompt_template)
 
-query = st.text_input("Enter your question")
-# qa.run(query)
-# query = st.text_input("Type your message here")
-# qu = 
-if query is not None:
-# response = 
-    st.write(qa.run(query))
+    # Chain 2: Generating assumptions made in the statement
+    template = """Here is a statement:
+        {statement}
+        Make a bullet point list of the assumptions you made when producing the above statement.\n\n"""
+    prompt_template = PromptTemplate(input_variables=["statement"], template=template)
+    assumptions_chain = LLMChain(llm=llm, prompt=prompt_template)
+    assumptions_chain_seq = SimpleSequentialChain(
+        chains=[question_chain, assumptions_chain], verbose=True
+    )
+
+    # Chain 3: Fact checking the assumptions
+    template = """Here is a bullet point list of assertions:
+    {assertions}
+    For each assertion, determine whether it is true or false. If it is false, explain why.\n\n"""
+    prompt_template = PromptTemplate(input_variables=["assertions"], template=template)
+    fact_checker_chain = LLMChain(llm=llm, prompt=prompt_template)
+    fact_checker_chain_seq = SimpleSequentialChain(
+        chains=[question_chain, assumptions_chain, fact_checker_chain], verbose=True
+    )
+
+    # Final Chain: Generating the final answer to the user's question based on the facts and assumptions
+    template = """In light of the above facts, how would you answer the question '{}'""".format(
+        user_question
+    )
+    template = """{facts}\n""" + template
+    prompt_template = PromptTemplate(input_variables=["facts"], template=template)
+    answer_chain = LLMChain(llm=llm, prompt=prompt_template)
+    overall_chain = SimpleSequentialChain(
+        chains=[question_chain, assumptions_chain, fact_checker_chain, answer_chain],
+        verbose=True,
+    )
+
+    # Running all the chains on the user's question and displaying the final answer
+    st.success(overall_chain.run(user_question))
