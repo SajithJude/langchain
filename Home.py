1f05365ebb606fee1f5fbaa43d7ae3b43a49e6bd
Jude Sajith <judesajith.aj@gmail.com>
2023-03-23 03:28:51 -0400

    msmss

---
+++ Home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from langchain.document_loaders import PyPDFLoader
+from langchain.indexes import VectorstoreIndexCreator
+import os
+import openai
+
+openai.api_key = os.getenv("API_KEY")
+
+
+loader = PyPDFLoader("content\Treasury Management Book .pdf")
+pages = loader.load_and_split()
+index = VectorstoreIndexCreator().from_loaders([loader])
+
+
+
+query = st.text_input("Type your message here")
+
+if query is not None:
+    response = index.query(query)
+    st.write(response)
