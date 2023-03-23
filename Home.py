93ed96f45a928493f7fd9ec13a36c11805e70ee1
Jude Sajith <judesajith.aj@gmail.com>
2023-03-23 04:40:17 -0400

    sssss

--- Home.py
+++ Home.py
@@ -1,27 +1,30 @@
 import streamlit as st
 from langchain.document_loaders import PyPDFLoader
 from langchain.indexes import VectorstoreIndexCreator
+from langchain.chains import VectorDBQA
+from langchain.llms import OpenAI
 import os
 import openai
 
+
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+
+
+
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-try:
-    loader = PyPDFLoader("content/Treasury Management Book .pdf")
-    # pages = loader.load_and_split()
-    index = VectorstoreIndexCreator().from_loaders([loader])
 
-    qu = "What is treasury management"
-    response = index.query(str(qu),type=String)
-    st.write(qu)
+loader = PyPDFLoader("content/Treasury Management Book .pdf")
+# pages = loader.load_and_split()
+# index = VectorstoreIndexCreator().from_loaders([loader])
 
-except IndexError as e:
-    st.write("IndexError occurred:", e)
-    st.write("Creating VectorstoreIndex...")
-    loader = PyPDFLoader("content/Treasury Management Book .pdf")
-    # pages = loader.load_and_split()
-    index = VectorstoreIndexCreator().from_loaders([loader])
-    
-    qu = "What is treasury management"
-    response = index.query(str(qu))
-    st.write(qu)
+
+
+embeddings = OpenAIEmbeddings()
+db = Chroma.from_documents(loader, embeddings)
+
+
+qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=db)
+query = "What did the president say about Ketanji Brown Jackson"
+qa.run(query)
