e738583c56fe6357d6ebba1a14698cc99ccc1ea0
Jude Sajith <judesajith.aj@gmail.com>
2023-03-23 04:42:25 -0400

    ssss

--- Home.py
+++ Home.py
@@ -1,15 +1,10 @@
 import streamlit as st
 from langchain.document_loaders import PyPDFLoader
 from langchain.indexes import VectorstoreIndexCreator
-from langchain.chains import VectorDBQA
-from langchain.llms import OpenAI
 import os
 import openai
-
-
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-
+embeddings = OpenAIEmbeddings()
 
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -17,14 +12,12 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 loader = PyPDFLoader("content/Treasury Management Book .pdf")
 # pages = loader.load_and_split()
-# index = VectorstoreIndexCreator().from_loaders([loader])
+index = VectorstoreIndexCreator(vectorstore_cls=Chroma, 
+    embedding=OpenAIEmbeddings()).from_loaders([loader])
 
 
 
-embeddings = OpenAIEmbeddings()
-db = Chroma.from_documents(loader, embeddings)
+qu = "What is treasury management"
 
-
-qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=db)
-query = "What did the president say about Ketanji Brown Jackson"
-qa.run(query)
+response = index.query(str(qu))
+st.write(qu)
