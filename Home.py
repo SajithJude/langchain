6180c55518c9f3ef206df3442b2532f5fcb05e54
Jude Sajith <judesajith.aj@gmail.com>
2023-03-23 04:28:42 -0400

    ssss

--- Home.py
+++ Home.py
@@ -8,7 +8,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 try:
     loader = PyPDFLoader("content/Treasury Management Book .pdf")
-    pages = loader.load_and_split()
+    # pages = loader.load_and_split()
     index = VectorstoreIndexCreator().from_loaders([loader])
 
     qu = "What is treasury management"
@@ -19,7 +19,7 @@ except IndexError as e:
     st.write("IndexError occurred:", e)
     st.write("Creating VectorstoreIndex...")
     loader = PyPDFLoader("content/Treasury Management Book .pdf")
-    pages = loader.load_and_split()
+    # pages = loader.load_and_split()
     index = VectorstoreIndexCreator().from_loaders([loader])
     
     qu = "What is treasury management"
