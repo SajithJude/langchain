9da7ea9412e80946b43352d24f052a3ee5a61ec4
Jude Sajith <judesajith.aj@gmail.com>
2023-03-23 10:22:51 -0400

    snsnsns

--- Home.py
+++ Home.py
@@ -16,9 +16,9 @@ query = st.text_input("Enter your question")
 if st.button("Submit"):
     respones = index.query_with_sources(query)
     st.write(respones)
-    sources = responses.sources
+    sources = responses['sources']
     st.write(sources)
-    answer = st.write(respones.answer)
+    answer = st.write(respones['answer'])
 
 
 
