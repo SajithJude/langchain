f5e9cb48b5ee624aa9e5edc9f1930046633ad608
Jude Sajith <judesajith.aj@gmail.com>
2023-03-23 05:05:54 -0400

    ssss

--- Home.py
+++ Home.py
@@ -1,20 +1,30 @@
 import streamlit as st
-from langchain.document_loaders import PyPDFLoader
-from langchain.indexes import VectorstoreIndexCreator
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import CharacterTextSplitter
+from langchain import OpenAI, VectorDBQA
+
 import os
 import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-loader = PyPDFLoader("content/Treasury Management Book .pdf")
-pages = loader.load_and_split()
-index = VectorstoreIndexCreator().from_loaders([loader])
+from langchain.document_loaders import TextLoader
+loader = TextLoader('/content/inputenglish.txt')
+documents = loader.load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+texts = text_splitter.split_documents(documents)
 
+embeddings = OpenAIEmbeddings()
+docsearch = Chroma.from_documents(texts, embeddings)
+qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=docsearch)
 
-
+query = "what is this book about"
+qa.run(query)
 # query = st.text_input("Type your message here")
 # qu = 
 
 # response = 
-st.write(index.query("What is treasury management"))
+st.write(qa.run(query))
